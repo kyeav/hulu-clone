d8c85eb0631a169d111420394eb607fbb94e7620
Kylie <kylieavie@gmail.com>
2022-10-05 20:23:55 +0800

    results page final changes

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
 import Results from '../components/Results'
+import requests from '../utils/requests'
 
 // client
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -14,7 +15,7 @@ export default function Home() {
 
       <Header />
       <Nav />
-      <Results />
+      <Results results={results} />
     </div>
   )
 }
@@ -23,5 +24,14 @@ export default function Home() {
 export async function getServerSideProps(context) {
   const genre = context.query.genre
 
-  
+  const request = await fetch(`https://api.themoviedb.org/3${
+    requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then(res => res.json())
+
+  return {
+    props: {
+      results: request.results,
+    },
+  }
 }
