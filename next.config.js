85e3c810d001480402e1a07a8b8b363656b20071
Kylie <kylieavie@gmail.com>
2022-10-17 14:14:11 +0800

    Ignoring ESLint

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     images: {
       domains: ["links.papareact.com", "image.tmdb.org"],
-    }
+    },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
   }
   
 //   /** @type {import('next').NextConfig} */
